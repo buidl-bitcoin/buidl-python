6f5faf08d77b50a86c62d15d48afa4bc1451cde0
Michael Flaxman <448644+mflaxman@users.noreply.github.com>
2020-10-22 11:08:46 -0500

    add to pypi (#3)

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="buidl",
+    version="0.1.0",
+    author="Example Author",
+    author_email="author@example.com",
+    description="An easy-to-use and fully featured bitcoin library written in pure python (no dependencies).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/buidl-bitcoin/buidl-python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
