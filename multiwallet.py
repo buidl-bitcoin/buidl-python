6f85a34aec44e6096849de12412b0f6a867538cd
Michael Flaxman <mflaxman@gmail.com>
2020-10-27 12:22:16 -0500

    stashing changes

--- multiwallet.py
+++ multiwallet.py
@@ -1,13 +1,18 @@
+import re
 import readline
 
 from cmd import Cmd
 
-from buidl.hd import HDPrivateKey
+from buidl.hd import HDPrivateKey, HDPublicKey
+from buidl.helper import sha256
 from buidl.mnemonic import WORD_LIST, WORD_LOOKUP
+from buidl.script import P2WSHScriptPubKey, WitnessScript
+from buidl.op import OP_CODE_NAMES_LOOKUP
+
+readline.parse_and_bind("tab: complete")
+
 
 # https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-python
-
-
 class colors:
     """Colors class:
     reset all colors with colors.reset
@@ -55,6 +60,10 @@ class colors:
         lightgrey = "\033[47m"
 
 
+def blue_fg(string):
+    return f"{colors.fg.blue}{string}{colors.reset}"
+
+
 def yellow_fg(string):
     return f"{colors.fg.yellow}{string}{colors.reset}"
 
@@ -91,43 +100,102 @@ class WordCompleter:
         return results[state] + " "
 
 
-readline.parse_and_bind("tab: complete")
+def _re_pubkey_info_from_descriptor_fragment(fragment):
+    xfp, path, xpub, idx = re.match(
+        "\[([0-9a-f]+)\*?(.*?)\]([0-9A-Za-z]+).*([0-9]+?)",  # noqa: W605
+        fragment,
+    ).groups()
+    return {
+        "xfp": xfp,
+        "path": path.replace("\\/", "/").lstrip("/"),
+        "xpub": xpub,
+        "idx": int(idx),
+    }
 
 
-def _get_bip39_firstwords():
+def _get_pubkeys_info_from_descriptor(descriptor):
+    re_results = re.findall("wsh\(sortedmulti\((.*)\)\)", descriptor)  # noqa: W605
+    parts = re_results[0].split(",")
+    quorum_m = int(parts.pop(0))
+    quorum_n = len(parts)  # remaining entries are pubkeys with fingerprint/path
+    assert 0 < quorum_m <= quorum_n
+
+    pubkey_dicts = []
+    for fragment in parts:
+        pubkey_info = _re_pubkey_info_from_descriptor_fragment(fragment=fragment)
+        parent_pubkey_obj = HDPublicKey.parse(pubkey_info["xpub"])
+        pubkey_info["parent_pubkey_obj"] = parent_pubkey_obj
+        pubkey_info["child_pubkey_obj"] = parent_pubkey_obj.child(
+            index=pubkey_info["idx"]
+        )
+        pubkey_dicts.append(pubkey_info)
+
+    # safety check
+    all_pubkeys = [x["xpub"] for x in pubkey_dicts]
+    assert (
+        len(set([x[:4] for x in all_pubkeys])) == 1
+    ), "ERROR: multiple conflicting networks in pubkeys: {}".format(all_pubkeys)
+
+    return {
+        "quorum_m": quorum_m,
+        "quorum_n": quorum_n,
+        "pubkey_dicts": pubkey_dicts,
+    }
+
+
+def _get_bip39_checksumwords():
     old_completer = readline.get_completer()
     completer = WordCompleter(wordlist=WORD_LIST)
 
     readline.set_completer(completer.complete)
-    fw = input("Enter your 23 word BIP39 seed phrase: ").strip()
+    fw = input(blue_fg("Enter your 23 word BIP39 seed phrase: ")).strip()
     fw_num = len(fw.split())
     if fw_num not in (11, 14, 17, 20, 23):
         # TODO: 11, 14, 17, or 20 word seed phrases also work but this is not documented as it's for advanced users
         print(red_fg(f"Enter 23 word seed-phrase (you entered {fw_num} words)"))
-        return _get_bip39_firstwords()
+        return _get_bip39_checksumwords()
     for cnt, word in enumerate(fw.split()):
         if word not in WORD_LOOKUP:
             print(red_fg(f"Word #{cnt+1} ({word} is not a valid BIP39 word"))
-            return _get_bip39_firstwords()
+            return _get_bip39_checksumwords()
     valid_checksum_words, err_str = get_all_valid_checksum_words(fw)
     if err_str:
         print(red_fg(f"Error calculating checksum word: {err_str}"))
-        return _get_bip39_firstwords()
+        return _get_bip39_checksumwords()
 
     readline.set_completer(old_completer)
     return fw, valid_checksum_words
 
 
-def _is_testnet():
-    network = input("Network [Test/main]: ")
-    if network.strip() == "" or network.strip().lower() in ("test", "testnet"):
-        return True
-    if network.strip().lower() in ("main", "mainet", "mainnet"):
-        return False
+def _get_network():
+    network = input(blue_fg("Network [Test/main]: ")).strip().lower()
+    if network == "" or network in ("test", "testnet"):
+        return "Testnet"
+    if network in ("main", "mainet", "mainnet"):
+        return "Mainnet"
 
     # Bad input, ask again
     print(red_fg("Please choose either test or main"))
-    return _is_testnet()
+    return _get_network()
+
+
+def _get_int(prompt, default=20, minimum=0, maximum=20):
+    res = input(blue_fg(f"{prompt} [{default}]: ")).strip()
+    if not res:
+        res = str(default)
+    try:
+        res_int = int(res)
+    except ValueError:
+        print(red_fg(f"{res} is not an integer"))
+        return _get_int(
+            prompt=prompt, default=default, minimum=minimum, maximum=maximum
+        )
+    if not minimum <= res_int <= maximum:
+        print(red_fg(f"{res_int} must be between {minimum} and {maximum}"))
+        return _get_int(
+            prompt=prompt, default=default, minimum=minimum, maximum=maximum
+        )
+    return res_int
 
 
 class MyPrompt(Cmd):
@@ -136,51 +204,86 @@ class MyPrompt(Cmd):
 
     def do_seedpicker(self, arg):
         """Calculate bitcoin public and private key information from BIP39 words drawn out of a hat"""
-        is_testnet = _is_testnet()
-        print("is_testnet", yellow_fg(is_testnet))
-        first_words, valid_checksum_words = _get_bip39_firstwords()
-        print("words entered: ", first_words)
+        network = _get_network()
+        first_words, valid_checksum_words = _get_bip39_checksumwords()
 
-        print(f"Last word: {valid_checksum_words[0]}")
-
-        if is_testnet:
-            PATH = "m/48'/1'/0'/2'"
-            SLIP132_VERSION_BYTES = "02575483"
-        else:
+        if network == "Mainnet":
             PATH = "m/48'/0'/0'/2'"
             SLIP132_VERSION_BYTES = "02aa7ed3"
+        elif network == "Testnet":
+            PATH = "m/48'/1'/0'/2'"
+            SLIP132_VERSION_BYTES = "02575483"
 
         hd_priv = HDPrivateKey.from_mnemonic(
             first_words + " " + valid_checksum_words[0]
         )
-        xfp = hd_priv.fingerprint().hex()
 
-        print("SECRET INFO")
+        print(green_fg("SECRET INFO") + red_fg(" guard this very carefully"))
+        print(green_fg(f"Calculated last word: {valid_checksum_words[0]}"))
         print(
-            "Full mnemonic (with checksum word):",
-            first_words + " " + valid_checksum_words[0],
+            green_fg(
+                f"Full ({len(first_words.split()) + 1} word) mnemonic with last word: {first_words + ' ' + valid_checksum_words[0]}"
+            )
         )
-        print("Full mnemonic length (# words):", len(first_words.split()) + 1)
 
         print("")
-        print("PUBLIC KEY INFO")
-        print("Network:", "Testnet" if is_testnet else "Mainnet")
+        print(green_fg("PUBLIC KEY INFO"))
+        print(green_fg(f"Network: {network}"))
 
-        child_slip132_pubkey = hd_priv.traverse(PATH).xpub(
-            version=bytes.fromhex(SLIP132_VERSION_BYTES)
-        )
-        if False:
-            # TODO: add verbosity flag
-            print("xfp:", xfp)
-            print("path:", PATH)
-            print("child_slip132_pubkey:", child_slip132_pubkey)
-
-        print("Specter-Desktop Input Format:")
+        print(green_fg("Specter-Desktop Input Format:"))
         print(
-            "  [{}{}]{}".format(
-                xfp, PATH.replace("m", "").replace("'", "h"), child_slip132_pubkey
-            ),
+            green_fg(
+                "  [{}{}]{}".format(
+                    hd_priv.fingerprint().hex(),
+                    PATH.replace("m", "").replace("'", "h"),
+                    hd_priv.traverse(PATH).xpub(
+                        version=bytes.fromhex(SLIP132_VERSION_BYTES)
+                    ),
+                ),
+            )
         )
+
+    def do_verify_receive_address(self, arg):
+        """Verify receive addresses for a multisig wallet (using output descriptors from Specter-Desktop)"""
+        output_descriptor = input(
+            blue_fg("Paste in your output descriptor from Specter-Desktop: ")
+        ).strip()
+        pubkeys_info = _get_pubkeys_info_from_descriptor(descriptor=output_descriptor)
+        limit = _get_int(
+            prompt="Limit of addresses to generate",
+            default=20,
+            minimum=1,
+            maximum=10 ** 10,
+        )
+        offset = _get_int(
+            prompt="Offset of addresses to generate - EXPERTS ONLY",
+            default=0,
+            minimum=0,
+            maximum=10 ** 10,
+        )
+
+        for cnt in range(limit):
+            sec_hexes_to_use = []
+            for pubkey_info in pubkeys_info["pubkey_dicts"]:
+                # import pdb; pdb.set_trace()
+                leaf_xpub = pubkey_info["child_pubkey_obj"].child(index=cnt + offset)
+                sec_hexes_to_use.append(leaf_xpub.sec().hex())
+
+            commands = [OP_CODE_NAMES_LOOKUP["OP_{}".format(pubkeys_info["quorum_m"])]]
+            commands.extend(
+                [bytes.fromhex(x) for x in sorted(sec_hexes_to_use)]
+            )  # BIP67
+            commands.append(
+                OP_CODE_NAMES_LOOKUP["OP_{}".format(pubkeys_info["quorum_n"])]
+            )
+            commands.append(OP_CODE_NAMES_LOOKUP["OP_CHECKMULTISIG"])
+            witness_script = WitnessScript(commands)
+            redeem_script = P2WSHScriptPubKey(sha256(witness_script.raw_serialize()))
+            print(f"Address #{cnt + offset}: {redeem_script.address(testnet=True)}")
+
+    def do_sign_psbt(self, arg):
+        """Sign a PSBT from Specter-Desktop using one of your mnemonics"""
+        pass
 
     def do_exit(self, arg):
         """Exit Program"""
