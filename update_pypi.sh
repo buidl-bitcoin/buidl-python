93e65b2b3c238b5c02bc9da47f734e607c26bed9
Michael Flaxman <448644+mflaxman@users.noreply.github.com>
2020-11-14 22:01:19 -0600

    Bump version to V0.2.1 (#22)
    * bump version
    
    * update pypi script
    
    * don't require dependencies
    
    Co-authored-by: Michael Flaxman <mflaxman@gmail.com>

--- update_pypi.sh
+++ update_pypi.sh
@@ -1,1 +1,56 @@
-date && black --check . && pytest -v && flake8 . && git push && python3 setup.py sdist bdist_wheel && python3 -m pip install --upgrade twine && python3 -m twine upload dist/* && rm -rf dist/ && date
+#! /usr/bin/env bash
+
+# Verbose printing
+set -o xtrace
+
+# Exit virtualenv if we're in one
+deactivate
+
+# Abandon if anything errors
+set -e;
+
+# Remove old files
+rm -rf .venv3/
+rm -rf dist/
+rm -rf build/
+rm -rf buidl.egg-info/
+find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+
+# Tests
+black --check .
+flake8 .
+# pytest -v
+
+# Safety
+git push
+
+# Virtualenv
+python3 --version
+# Install virtualenv (if not installed)
+# python3 -m pip uninstall virtualenv -y
+python3 -m pip install virtualenv
+# Create virtualenv and install our software inside it
+python3 -m virtualenv .venv3
+source .venv3/bin/activate
+# python3 -m pip uninstall pyinstaller -y
+if [ -f requirements.txt ]; then python3 -m pip install -r requirements.txt; fi
+python3 setup.py install
+python3 -m pip freeze
+# Package
+python3 setup.py sdist bdist_wheel
+# Upload to PyPI
+python3 -m pip install --upgrade twine
+python3 -m twine upload dist/*
+
+# Cleanup
+rm -rfv dist/
+rm -rfv buidl.egg-info/
+rm -rfv build/
+find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+
+# Hackey timer
+# https://askubuntu.com/questions/1028924/how-do-i-use-seconds-inside-a-bash-script
+hrs=$(( SECONDS/3600 ))
+mins=$(( (SECONDS-hrs*3600)/60))
+secs=$(( SECONDS-hrs*3600-mins*60 ))
+printf 'Time spent: %02d:%02d:%02d\n' $hrs $mins $secs
