e6e492c04bc28ff85190c7f56148138f42846e53
Michael Flaxman <mflaxman@gmail.com>
2020-11-30 14:10:23 -0600

    add to tests and assert num_bits value makes sense

--- buidl/test/test_mnemonic.py
+++ buidl/test/test_mnemonic.py
@@ -6,6 +6,20 @@ from buidl.hd import HDPrivateKey
 
 class MnemonicTest(TestCase):
     def test_secure_mnemonic(self):
-        for num_bits in (128, 160, 192, 224, 256):
+        tests = (
+            # num_bits, num_words
+            (128, 12),
+            (160, 15),
+            (192, 18),
+            (224, 21),
+            (256, 24),
+        )
+
+        for num_bits, num_words in tests:
             mnemonic = secure_mnemonic(num_bits=num_bits)
+            self.assertEqual(num_words, len(mnemonic.split(" ")))
             HDPrivateKey.from_mnemonic(mnemonic, testnet=True)
+
+        for invalid_num_bits in (1, 127, 129, 257):
+            with self.assertRaises(AssertionError):
+                secure_mnemonic(num_bits=invalid_num_bits)
