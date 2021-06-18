e017ffc83961a525928ac0e5d84a95e87a6d731f
Michael Flaxman <mflaxman@gmail.com>
2021-06-17 18:14:22 -0600

    test less and spit out more

--- test_multiwallet.py
+++ test_multiwallet.py
@@ -15,9 +15,7 @@ class MultiwalletTest(unittest.TestCase):
                 # This will error out at the end of the buffer
                 latest_char = self.child.read(1)
             except Exception as e:
-                raise Exception(
-                    f"Failed to find `{text}` in `{buffer}`.\nGot error: `{e}`"
-                )
+                raise Exception(f"Failed to find text in `{buffer}`")
 
             try:
                 buffer += latest_char.decode()
