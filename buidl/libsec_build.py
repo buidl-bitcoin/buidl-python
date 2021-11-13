7b05aa45ed499f85a41ee71b62743785d3f7e85a
Michael Flaxman <mflaxman@gmail.com>
2021-11-12 22:03:13 -0600

    brew support for libsec

--- buidl/libsec_build.py
+++ buidl/libsec_build.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from cffi import FFI
 
 
--- clean.sh
+++ clean.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/env bash
 
 set -o xtrace
+pip3 uninstall buidl -y
 rm -rf .venv3/
 rm -rf dist/
 rm -rf build/
 rm -rf buidl.egg-info/
+rm buidl/_libsec.c
+rm buidl/_libsec.cpython-*-darwin.so
+rm buidl/_libsec.o
+rm buidl/_libsec.so
+
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
