1f01ffd26525d19b4852cfc9ed1003cc4d2b764e
tdb3 <106488469+tdb3@users.noreply.github.com>
2022-08-19 11:10:45 -0400

    Changed sendrawtransaction to classmethod

--- buidl/tx.py
+++ buidl/tx.py
@@ -88,13 +88,13 @@ class TxFetcher:
             s = json.dumps(to_dump, sort_keys=True, indent=4)
             f.write(s)
             
-    @staticmethod
-    def sendrawtransaction(signed_tx_hex_str, network="mainnet"):
+    @classmethod
+    def sendrawtransaction(cls, signed_tx_hex_str, network="mainnet"):
         """Broadcasts a signed transaction."""
         url = "{}/tx".format(TxFetcher.get_url(network=network))
         req = Request(url, data=signed_tx_hex_str.encode(), headers={"User-Agent": "Mozilla/5.0"})
         response = urlopen(req)
-        return response.read().decode("utf-8") # returns the txid if successful
+        return response.read().decode("utf-8")  # returns the txid if successful
 
 
 class Tx:
